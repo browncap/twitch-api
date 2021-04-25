a948041735acc455c2c9178bca607c9d26c865aa
Anechoic <17820200+browncap@users.noreply.github.com>
2021-04-25 12:15:25 -0500

    update config

--- src/main/scala/config/models.scala
+++ src/main/scala/config/models.scala
@@ -1,13 +1,7 @@
 package com.twitch.config
 
 final case class ServiceConfig(
-  http: HttpConfig,
   twitch: TwitchConfig
-)
-
-final case class HttpConfig(
-  port: Int,
-  host: String
 )
 
 final case class TwitchConfig(
