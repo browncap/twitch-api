01ed2c5afdbbb5229f81ee9680757e27f8b9c152
Anechoic <17820200+browncap@users.noreply.github.com>
2021-04-25 12:18:57 -0500

    remove unused model

--- src/main/scala/modules/twitch/models.scala
+++ src/main/scala/modules/twitch/models.scala
@@ -1,17 +1,8 @@
 package com.twitch.integration
 
-import io.circe.config.syntax._
 import com.twitch.token.AccessToken
 import io.circe.Decoder
 import io.circe.generic.semiauto._
-import io.circe.generic.extras.semiauto.deriveUnwrappedDecoder
-
-import scala.concurrent.duration.FiniteDuration
-
-final case class TokenExpiration(value: FiniteDuration)
-object TokenExpiration {
-  implicit val decoder: Decoder[TokenExpiration] = deriveUnwrappedDecoder
-}
 
 final case class AuthResponse(access_token: AccessToken)
 object AuthResponse {
